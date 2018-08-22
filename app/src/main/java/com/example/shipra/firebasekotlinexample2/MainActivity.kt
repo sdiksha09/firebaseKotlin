a0f460a5ad88273e4228807fa3b9d0b64fcc333c
sdiksha09 <diksha.sharma1209@gmail.com>
2018-08-22 18:10:39 +0530

    first commit

---
+++ app/src/main/java/com/example/shipra/firebasekotlinexample2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.shipra.firebasekotlinexample2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
